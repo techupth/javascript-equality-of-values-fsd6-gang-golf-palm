4576b978535724bfcc1acb9d71ba62e6e9ca7cd1
Krittayapornn <krittayaporn.ww@gmail.com>
2024-05-15 13:14:12 +0700

    fix: add comment answer ex1-2

--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,7 @@
 // Exercise #2: Loose Equality
 // Start commenting here
-0 == false;
-null == undefined;
-10 == "10";
-[10] == 10;
-[1,2] == "1,2";
+0 == false; //True
+null == undefined; //True
+10 == "10"; //True
+[10] == 10; //True
+[1,2] == "1,2"; //True
